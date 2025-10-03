fcbcac1d5fae595ef9367962ef70314bf655f24c
Nate Babyak <nate.babyak@outlook.com>
2025-10-03 09:51:21 -0400

    update middleware to allow static assets

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -15,3 +15,9 @@ export default auth((req) => {
 
   return NextResponse.next();
 });
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
