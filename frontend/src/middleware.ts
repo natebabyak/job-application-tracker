c761d6c333ea0b30bd5e5e5b18d354b76e30e4bf
Nate Babyak <nate.babyak@outlook.com>
2025-10-03 10:06:13 -0400

    update middleware to allow api/auth

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -18,6 +18,6 @@ export default auth((req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|api/auth|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
